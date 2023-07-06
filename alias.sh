94483ed491040fc4a06ea2ef4f12936821f0a268
miyago9267 <miyagomizugen@gmail.com>
2023-07-06 20:57:56 +0800

    too many

--- alias.sh
+++ alias.sh
@@ -4,12 +4,13 @@ alias cp='cp -i'
 alias mv='mv -i'
 alias l='ls -l'
 alias ll='ls -alF'
-alias py='python3'
+alias py='python3.10'
 alias nv='nvim'
 alias clr='clear'
 alias sudo='sudo '
-alias rc='~/dotfile/script/rc '
+alias rc='bash ~/dotfile/script/rc.sh '
 alias lnk='~/dotfile/script/lnk.sh '
+alias codegpt='~/lib/codegpt'
 
 # enable ls color
 if [ -x /usr/bin/dircolors ]; then
