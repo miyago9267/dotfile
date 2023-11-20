54b439835e2015690becfee0435943853656616c
miyago9267 <miyagomizugen@gmail.com>
2023-11-20 22:00:49 +0800

    delete: remove something useless

--- script/lnk.sh
+++
@@ -1,9 +0,0 @@
-case=$1
-file="cpp"
-file=$2
-dir=$(pwd)
-path="/public/miyago/ContestWriteUp/uva"
-if [ ! -f "${path}/other/${case}/sol.${2}" ]; then
-    touch $path/other/$case/sol.$file
-fi
-ln -s $path/other/$case/sol.$file /$dir/sol.$file
